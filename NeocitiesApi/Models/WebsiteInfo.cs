c770677e0515bd06cc71ddabaad1e667c299ff3e
Jonathan Markman <jmarkman446@gmail.com>
2020-08-01 02:01:48 -0400

    add get filelist and get site info methods

---
+++ NeocitiesApi/Models/WebsiteInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace NeocitiesApi.Models
+{
+    public class WebsiteInfo
+    {
+        [JsonProperty("result")]
+        public string Result { get; set; }
+        [JsonProperty("info")]
+        public Attributes Attributes { get; set; }
+    }
+
+    public class Attributes
+    {
+        [JsonProperty("sitename")]
+        public string SiteName { get; set; }
+        [JsonProperty("hits")]
+        public int NumberOfHits { get; set; }
+        [JsonProperty("created_at")]
+        public string CreatedAt { get; set; }
+        [JsonProperty("last_updated")]
+        public string LastUpdated { get; set; }
+        [JsonProperty("domain")]
+        public string Domain { get; set; }
+        [JsonProperty("tags")]
+        public List<string> Tags { get; set; }
+    }
+
+}
--- NeocitiesApi/NeocitiesApi.cs
+++ NeocitiesApi/NeocitiesApi.cs
@@ -1,6 +1,8 @@
 ﻿using NeocitiesApi.Models;
+using Newtonsoft.Json;
 using System;
 using System.Linq.Expressions;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -18,9 +20,73 @@ namespace NeocitiesApi
             _httpClient = CreateHttpClient(apiKey);
         }
 
-        public async Task<WebsiteFileList> GetWebsiteFileListAsync()
+        /// <summary>
+        /// Retrieves the list of all files and folders associated with the website. 
+        /// If a folder name is provided as an argument, a list of files and folders
+        /// in the specified folder will be retrieved.
+        /// </summary>
+        /// <param name="remotePath">A specific folder on the website backend</param>
+        /// <returns>A <see cref="WebsiteFileList"/> object containing the files</returns>
+        public async Task<WebsiteFileList> GetWebsiteFileListAsync(string remotePath = "")
         {
+            WebsiteFileList fileList;
+            string urlParams;
 
+            if (!string.IsNullOrWhiteSpace(remotePath))
+            {
+                urlParams = $"list?path={remotePath}";
+            }
+            else
+            {
+                urlParams = "list";
+            }
+
+            var response = await _httpClient.GetAsync(urlParams);
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                fileList = JsonConvert.DeserializeObject<WebsiteFileList>(data);
+            }
+            else
+            {
+                throw new WebException("Could not retrieve the list of files for the website");
+            }
+
+            return fileList;
+        }
+
+        /// <summary>
+        /// Retrieves metadata about a specified website. If no argument is provided,
+        /// the metadata about the website associated with the API key will be retrieved.
+        /// </summary>
+        /// <param name="siteName">A specific site (i.e., the "foobar" from "foobar.neocities.org")</param>
+        /// <returns>A <see cref="WebsiteInfo"/> object containing the site's metadata</returns>
+        public async Task<WebsiteInfo> GetWebsiteMetaDataAsync(string siteName = "")
+        {
+            WebsiteInfo info;
+            string urlParams;
+
+            if (!string.IsNullOrWhiteSpace(siteName))
+            {
+                urlParams = $"info?sitename={siteName}";
+            }
+            else
+            {
+                urlParams = "info";
+            }
+
+            var response = await _httpClient.GetAsync(urlParams);
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                info = JsonConvert.DeserializeObject<WebsiteInfo>(data);
+            }
+            else
+            {
+                throw new WebException("Could not retrieve the metadata for the website");
+            }
+
+            return info;
         }
 
         /// <summary>
@@ -31,7 +97,7 @@ namespace NeocitiesApi
         private HttpClient CreateHttpClient(string apiKey)
         {
             var client = new HttpClient();
-            client.BaseAddress = new Uri("https://neocities.org/api");
+            client.BaseAddress = new Uri("https://neocities.org/api/");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
 
             return client;
